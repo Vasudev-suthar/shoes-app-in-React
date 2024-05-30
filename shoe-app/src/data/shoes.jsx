ec12d560eda946b2e5e0e9fe8a4f3e65eabb3297
Vasudev suthar <doyalvasudev87@gmail.com>
2024-05-30 15:35:51 -0500

    change path of images folder

--- shoe-app/src/data/shoes.jsx
+++ shoe-app/src/data/shoes.jsx
@@ -4,49 +4,49 @@ const shoes = [
       id: 1,
       name: 'White Casual Sneaker',
       price: 70,
-      image: './images/casual-shoes.webp'
+      image: './src/images/casual-shoes.webp'
     },
     {
       id: 2,
       name: `MACTREE Men's Mid Top Ankle Boots`,
       price: 90,
-      image: './images/ankleshoe.jpg'
+      image: './src/images/ankleshoe.jpg'
     },
     {
       id: 3,
       name: `Campus Men's Sneakers`,
       price: 50,
-      image: './images/cumpus-1.png'
+      image: './src/images/cumpus-1.png'
     },
     {
       id: 4,
       name: `Campus Men's OG-03 Sneakers`,
       price: 75,
-      image: './images/cumpus-2.jfif'
+      image: './src/images/cumpus-2.jfif'
     },
     {
       id: 5,
       name: `U.S. POLO ASSN. Men's PanalSneaker`,
       price: 99,
-      image: './images/us-polo.png'
+      image: './src/images/us-polo.png'
     },
     {
       id: 6,
       name: `ASIAN Men's Sports Shoes`,
       price: 68,
-      image: './images/asian.jfif'
+      image: './src/images/asian.jfif'
     },
     {
       id: 7,
       name: `ASIAN Mens Thar-13 Sneaker`,
       price: 120,
-      image: './images/asian.jpg'
+      image: './src/images/asian.jpg'
     },
     {
       id: 8,
       name: `ASIAN Men's AIRWEAVE-02 Sports Shoes`,
       price: 40,
-      image: './images/airwaeve.jpg'
+      image: './src/images/airwaeve.jpg'
     }
   ];
   
